ae052f6ce2b605d25421b0e9eefc92bb5d310de2
davidM <victor9966@163.com>
2014-10-22 02:51:35 -0500

    Finished
    Accounting Done!

--- gen/dmart/app_kernel/R.java
+++ gen/dmart/app_kernel/R.java
@@ -1091,7 +1091,6 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050042;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1115,6 +1114,7 @@ containing a value of this type.
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
+        public static final int list_ip_seller=0x7f050040;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
@@ -1141,7 +1141,6 @@ containing a value of this type.
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
         public static final int text_ip=0x7f05003c;
-        public static final int text_ip_seller=0x7f050040;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1203,10 +1202,6 @@ containing a value of this type.
         public static final int activity_seller=0x7f03001a;
         public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
-    public static final class menu {
-        public static final int main=0x7f0c0000;
-        public static final int seller=0x7f0c0001;
-    }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
          */
@@ -1248,19 +1243,14 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
-        public static final int button_name_buyer_connect=0x7f0a0015;
-        public static final int button_name_main_buyer=0x7f0a0010;
-        public static final int button_name_main_seller=0x7f0a000f;
-        public static final int button_name_seller_start=0x7f0a0016;
-        public static final int connected=0x7f0a0018;
-        public static final int connecting=0x7f0a0017;
-        public static final int disconnected=0x7f0a0019;
-        public static final int text_view_buyer=0x7f0a0013;
-        public static final int text_view_seller=0x7f0a0014;
-        public static final int title_activity_buyer=0x7f0a0012;
-        public static final int title_activity_seller=0x7f0a0011;
+        public static final int button_name_buyer_connect=0x7f0a0012;
+        public static final int button_name_main_buyer=0x7f0a000f;
+        public static final int button_name_main_seller=0x7f0a000e;
+        public static final int button_name_seller_refresh=0x7f0a0014;
+        public static final int button_name_seller_start=0x7f0a0013;
+        public static final int text_view_buyer=0x7f0a0010;
+        public static final int text_view_seller=0x7f0a0011;
     }
     public static final class style {
         /** 
--- src/dmart/app_kernel/Seller.java
+++ src/dmart/app_kernel/Seller.java
@@ -3,6 +3,7 @@ package dmart.app_kernel;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
@@ -10,29 +11,29 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
+import android.view.View.OnCreateContextMenuListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 
 public class Seller extends ActionBarActivity implements Handler.Callback {
 	private String sellerTAG = "Seller_Kernel";
 	
-	private TextView textview = null;
+	private ArrayList<String> ipList = null;
+	private ArrayList<Double> accountList = null;
+	private ArrayList<String> ipWithAccountList = null;
+	private ArrayAdapter<String> listAdapter = null;
+	private ListView listview = null;
 	
 	private Handler mHandler;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_seller);
-		
-		textview = (TextView)findViewById(R.id.text_ip_seller);
-		if (mHandler == null) {mHandler = new Handler(this);}
-	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -74,82 +75,165 @@ public class Seller extends ActionBarActivity implements Handler.Callback {
 		return super.onKeyDown(KeyCode, event);
 	}
 	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_seller);
+		
+		ipList = new ArrayList<String>();
+		accountList = new ArrayList<Double>();
+		ipWithAccountList = new ArrayList<String>();
+		
+		listAdapter = new ArrayAdapter<String>(this,
+					android.R.layout.simple_expandable_list_item_1, ipWithAccountList);
+		listview = (ListView)findViewById(R.id.list_ip_seller);
+		listview.setAdapter(listAdapter);
+		listviewOnItemLongClick();
+		
+		if (mHandler == null) {mHandler = new Handler(this);}
+	}
+	
+	private void listviewOnItemLongClick() {
+		listview.setOnCreateContextMenuListener(new OnCreateContextMenuListener() {
+			@Override
+			public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+				menu.add(Menu.NONE, 1, 1, "Delete this Buyer");
+				menu.add(Menu.NONE, 2, 2, "Cancel");
+			}
+			
+		});
+	}
+	
+	public boolean onContextItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+		case 1:
+			AdapterView.AdapterContextMenuInfo menuInfo;
+			menuInfo = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+			int pos = menuInfo.position;
+			String ip = ipList.get(pos);
+			Log.d(sellerTAG, "Seller remove Buyer (" + ipList.get(pos) + ")");
+			// first remove the buyer from whitelist
+			try {
+				Runtime mRuntime = Runtime.getRuntime();
+        		Process mProcess = mRuntime.exec("su -c iptables -D FORWARD -s " + ip + " -j ACCEPT");
+        		mProcess = mRuntime.exec("su -c iptables -D FORWARD -s " + ip + "/24 -j DROP");
+        		mProcess = mRuntime.exec("su -c iptables -t nat -D POSTROUTING -s " + ip + " -o wlan0 -j MASQUERADE");
+        		Toast.makeText(this, "Dis-Connect with Buyer " + ip, Toast.LENGTH_SHORT).show();
+			} catch (IOException e) {
+				Log.e(sellerTAG, "Seller Delete Buyer Failed: " + e.toString());
+			}
+			// then remove the buyer item from ListView
+			ipList.remove(pos); accountList.remove(pos);
+			ipWithAccountList.remove(pos);
+			listAdapter.notifyDataSetInvalidated();
+        	break;
+		case 2:
+			break;
+		}
+		return true;
+	}
+	
 	public void onClick1(View view) {
 		new Thread(new Runnable() {
             public void run() {
             	Runtime mRuntime = Runtime.getRuntime();
             	Process mProcess;
             	StringBuffer arpResBuff = null;
+            	StringBuffer accountResBuff = null;
             	String ip="", dev="";
             	// loop until get p2p-p2p0 interface address
             	Log.d(sellerTAG, "Begin to Fetch Wifi Direct Client IP");
-            	while(true) {
-            		boolean p2pFlag = false;
-	        		try {
-	        			mProcess = mRuntime.exec("arp -n");
-	        			BufferedReader arpReader = new BufferedReader(new InputStreamReader(mProcess.getInputStream()));
-	        			arpResBuff = new StringBuffer();
-	        			char[] mBuff = new char[1024];
-	        			int length = 0;
-	        			while((length=arpReader.read(mBuff)) != -1) {
-	        				arpResBuff.append(mBuff, 0, length);
-	        			}
-	        			arpReader.close();
-	        		} catch (IOException e) {
-	        			Log.e(sellerTAG, "SELLER EXCECUTE ARP FAILED: " + e.toString());
-	        		}
-	        		
-        			String[] arpItems = arpResBuff.toString().split("\n");
-        			for(int i=0;i<arpItems.length;++i) {
-        				if(arpItems[i].contains("p2p-p2p0")) {
-        					int devIndex = arpItems[i].indexOf("p2p-p2p");
-        					dev = arpItems[i].substring(devIndex);
-        					
-        					int gwIndex1 = arpItems[i].indexOf("(");
-        					int gwIndex2 = arpItems[i].indexOf(")");
-        					ip = arpItems[i].substring(gwIndex1+1, gwIndex2);
-        					p2pFlag = true;
-        					break;
-        				}
-        			}
-        			if(p2pFlag) {break;}
-            	}
-            	Log.d(sellerTAG, "SELLER GET BUYER IP: " + ip);
-            	Message msg = new Message();
-                Bundle b = new Bundle();
-                b.putString("message", ip);
-                msg.setData(b);
-                mHandler.sendMessage(msg);
-            	
-            	// block all the address and add the only whitelist
+
+            	// find connected Buyer's IP
             	try {
-            		// enable FORWARDING
-            		mProcess = mRuntime.exec("su -c sysctl -w net.ipv4.ip_forward=1");
-            		// su -c iptables -A FORWARD -s 192.168.49.1/24 -j DROP
-					mProcess = mRuntime.exec("su -c iptables -A FORWARD -s " + ip + "/24 -j DROP");
-					// su -c iptables -I FORWARD -s IP -j ACCEPT
-					mProcess = mRuntime.exec("su -c iptables -I FORWARD -s " + ip + " -j ACCEPT");
-				} catch (IOException e) {
-					Log.e(sellerTAG, "SELLER (UN)BLOCK FAILED: " + e.toString());
-				}
-            	Log.d(sellerTAG, "SELLER UNBLOCK BUYER IP: " + ip);
-            	msg = new Message();
-                b = new Bundle();
-                b.putString("message", ip+" unblocked");
-                msg.setData(b);
-                mHandler.sendMessage(msg);
-                
-                // add the NAT rules to relay packets from Buyer
-                try {
-                	// su -c iptables -t nat -I POSTROUTING -s 192.168.49.1/24 -o wlan0 -j MASQUERADE
-                	mProcess = mRuntime.exec("su -c iptables -t nat -I POSTROUTING -s " + ip + "/24 -o wlan0 -j MASQUERADE");
-                } catch (IOException e) {
-                	Log.e(sellerTAG, "SELLER ADD NAT RULES FAILED: " + e.toString());
-                }
-                Log.d(sellerTAG, "SELLER SNAT BUYER IP: " + ip);
-            	msg = new Message();
-                b = new Bundle();
-                b.putString("message", ip+" has source NAT");
+        			mProcess = mRuntime.exec("arp -n");
+        			BufferedReader arpReader = new BufferedReader(new InputStreamReader(mProcess.getInputStream()));
+        			arpResBuff = new StringBuffer();
+        			char[] mBuff = new char[1024];
+        			int length = 0;
+        			while((length=arpReader.read(mBuff)) != -1) {
+        				arpResBuff.append(mBuff, 0, length);
+        			}
+        			arpReader.close();
+        		} catch (IOException e) {
+        			Log.e(sellerTAG, "SELLER EXCECUTE ARP FAILED: " + e.toString());
+        		}
+            	
+            	// collect accounting information
+            	try {
+        			Process nProcess = mRuntime.exec("su -c iptables -L FORWARD -n -v -x");
+        			BufferedReader accountReader = new BufferedReader(new InputStreamReader(nProcess.getInputStream()));
+        			accountResBuff = new StringBuffer();
+        			char[] mBuff = new char[1024];
+        			int length = 0;
+        			while((length=accountReader.read(mBuff)) != -1) {
+        				accountResBuff.append(mBuff, 0, length);
+        			}
+        			accountReader.close();
+        		} catch (IOException e) {
+        			Log.e(sellerTAG, "SELLER EXCECUTE ARP FAILED: " + e.toString());
+        		}
+        		
+            	String[] arpItems = arpResBuff.toString().split("\n");
+            	String[] accountItems = accountResBuff.toString().split("\n");
+    			for(int i=0;i<arpItems.length;++i) {
+    				if(arpItems[i].contains("p2p-p2p0")) {
+    					int devIndex = arpItems[i].indexOf("p2p-p2p");
+    					dev = arpItems[i].substring(devIndex);
+    					
+    					int gwIndex1 = arpItems[i].indexOf("(");
+    					int gwIndex2 = arpItems[i].indexOf(")");
+    					ip = arpItems[i].substring(gwIndex1+1, gwIndex2);
+    					
+    					if(ipList.contains(ip)) {
+    						// an existed Buyer, update the traffic account
+    						Log.d(sellerTAG, "SELLER UPDATE BUYER ACCOUNT: " + ip);
+    						
+    						for(int j=0;j<accountItems.length;++j) {
+    							if(accountItems[j].contains(ip)) {
+    								String[] items = accountItems[j].split(" ");
+    								int count = 0;
+    								for(int k=0;k<items.length;++k) {
+    									if(items[k].length() == 0) {continue;}
+    									else {
+    										count += 1;
+    										if(count == 2) {
+    											Log.d(sellerTAG, "Bytes: " + items[k]);
+    											int ipIndex = ipList.indexOf(ip);
+    											accountList.set(ipIndex, Double.parseDouble(items[k]));
+    											ipWithAccountList.set(ipIndex, ip + "\t " + accountList.get(ipIndex) + " Bytes");
+    											break;
+    										}
+    									}
+    								}
+    								break;
+								}
+    						}
+    					} else {
+    						// a new Buyer is connected
+    						ipList.add(ip);
+    						accountList.add(0.0);
+    						ipWithAccountList.add(ip + "\t 0.0 Bytes");
+    						Log.d(sellerTAG, "SELLER GET NEW BUYER IP: " + ip);
+    						try {
+    		            		// enable FORWARDING
+    		            		mProcess = mRuntime.exec("su -c sysctl -w net.ipv4.ip_forward=1");
+    		            		// block all IP: su -c iptables -A FORWARD -s 192.168.49.1/24 -j DROP
+    							mProcess = mRuntime.exec("su -c iptables -A FORWARD -s " + ip + "/24 -j DROP");
+    							// add whitelist: su -c iptables -I FORWARD -s IP -j ACCEPT
+    							mProcess = mRuntime.exec("su -c iptables -I FORWARD -s " + ip + " -j ACCEPT");
+    							// add NAT rule: su -c iptables -t nat -I POSTROUTING -s 192.168.49.1/24 -o wlan0 -j MASQUERADE
+    		                	mProcess = mRuntime.exec("su -c iptables -t nat -I POSTROUTING -s " + ip + " -o wlan0 -j MASQUERADE");
+    						} catch (IOException e) {
+    							Log.e(sellerTAG, "SELLER ADD BUYER: " + e.toString());
+    						}
+    					}
+    				}
+    			}
+    			
+                Message msg = new Message();
+                Bundle b = new Bundle();
+                b.putString("message", "update");
                 msg.setData(b);
                 mHandler.sendMessage(msg);
             }
@@ -161,7 +245,8 @@ public class Seller extends ActionBarActivity implements Handler.Callback {
     	if (message != null) {
     		Bundle b = message.getData();
     		String msg = b.getString("message");
-    		textview.setText(msg);
+    		listAdapter.notifyDataSetChanged();
+    		Toast.makeText(this, "Buyer Accounts have been Updated!", Toast.LENGTH_LONG).show();
         }
         return true;
     }
